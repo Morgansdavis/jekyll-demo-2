b261378f9ccc0448f522979a053e6e2dc3b61ffd
Morgansdavis <morgansdavis@outlook.com>
2023-05-05 15:33:41 +0800

    popup gallery and if/then

--- scripts.js
+++ scripts.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
 
     // Animate on Scroll //
     AOS.init();
+
+    // Lightgallery //
+    lightGallery(document.getElementById('lightgallery'), {
+        speed: 500,
+        download: false
+    });
+
+    lightGallery(document.getElementById('lightgallery-2'), {
+        speed: 500,
+        download: false
+    });
+
 });
